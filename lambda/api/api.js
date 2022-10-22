cfe772fe8b12283ab56ccf6bcdb702d7440f6a69
Andrew Bellas <andrewsbellas@gmail.com>
2022-10-22 18:38:16 -0500

    Use Axios

--- lambda/api/api.js
+++ lambda/api/api.js
@@ -1,15 +1,15 @@
-import fetch from "./fetch.js";
 import ssm from "../ssm.js";
+import axios from "axios";
 
 export const fetchIPAPI = async (ipAddress) => {
   const key = await ssm.retrieve("IPAPIAccessKey", true);
   const url = `http://api.ipapi.com/${ipAddress}?access_key=${key}`;
-  return fetch.get(url);
+  return axios.get(url);
 };
 
 export const fetchVirusTotal = async (ipAddress) => {
   const key = await ssm.retrieve("VirusTotalAPIKey", true);
   const url = `http://www.virustotal.com/api/v3/ip_addresses/${ipAddress}`;
-  const result = await fetch.get(url, { headers: { "x-apikey": key } });
-  return result.data.attributes;
+  const result = await axios.get(url, { headers: { "x-apikey": key } });
+  return result.data.data.attributes;
 };
